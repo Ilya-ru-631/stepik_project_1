7d031639fd11d9c5a076d3e7f38d2482ffbf9602
sky <yuntcamokat@gmail.com>
2026-07-09 12:50:21 +0300

    Second variant. Print list dir and files

--- main.go
+++ main.go
@@ -9,32 +9,85 @@ package main
 // )
 
 import (
+	"fmt"
 	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
 )
 
-func dirTree(r io.Reader, path string, printFiles bool) error {
-	txt, err := os.OpenFile(path, os.O_RDWR, 0600)
-	if err != nil {
-		return err
-	}
-	defer txt.Close()
-
-	info, err := os.Stat(path)
-	if err != nil {
-		return nil
-	}
-
-	filesize := info.Size()
-	buf := make([]byte, filesize)
-
+func PrintList(path string, printFiles bool) ([]string, error) {
+	res := []string{}
+	root := path
 	if printFiles {
-		_, err := txt.WriteString(string(buf))
-		if err != nil {
+		err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if d.Name() == ".git" {
+				return filepath.SkipDir
+			}
+
+			inf, err := d.Info()
+			if err != nil {
+				return err
+			}
+
+			sizee := inf.Size()
+		
+			if d.IsDir() {
+				res = append(res, d.Name())
+				//fmt.Println(d.Name())
+			} else {
+				if sizee == 0 {
+					temp_str := fmt.Sprintf("\n%v - %s\n", d.Name(), "empty")
+					res = append(res, temp_str)
+					//fmt.Printf("\n%v - %s\n", d.Name(), "empty")
+				} else {
+					temp_str := fmt.Sprintf("\n%v - %d\n", d.Name(), sizee)
+					res = append(res, temp_str)
+					//fmt.Printf("\n%v - %d\n", d.Name(), sizee)
+				}
+			}
 			return nil
+		})
+		if err != nil {
+			return res, nil
+		}
+	} else {
+		err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if d.Name() == ".git" {
+				return filepath.SkipDir
+			}
+
+			if d.IsDir() {
+				res = append(res, d.Name(), "\n")
+				//fmt.Println(d.Name())
+			}
+
+			return nil
+		})
+		if err != nil {
+			return res, nil
 		}
 	}
 
+	return res, nil
+}
+
+func dirTree(w io.Writer, path string, printFiles bool) error {
+	res, err := PrintList(path, printFiles)
+	if err != nil {
+		return err
+	}
+	for _, file := range res {
+		if file.
+	}
 	return nil
 }
 
